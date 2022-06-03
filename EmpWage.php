884afb1aabbbbd804d991fa2d815a68c457867a6
unknown <bhumesh.ranjaneb598@gmail.com>
2022-06-03 12:47:23 +0530

    Added:Empwage Attendence

--- EmpWage.php
+++ EmpWage.php
@@ -3,8 +3,19 @@ class EmpWage{
      static function welcomeMsg(){
         echo "Welcome to Employee wage program \n";
     }
-    
-
-}
+    function empCheck(){
+       $present =1;
+        $number =rand(0,1);
+        if($number==$present){
+            echo "Employee is present";
+        }
+        else{
+            echo "Employee is Absent";
+        }
+ 
+    }
+ }
 EmpWage::welcomeMsg();
+$emp = new EmpWage();
+$emp->empCheck();
 ?>
